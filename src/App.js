c587ba03be1d975c32b59c29e2da5192431a701e
Zerg <amcfarlane52@gmail.com>
2019-10-04 12:35:25 -0700

    Running into some tabbing issues on pulling up the Resume. Took a step back to spend some time on design and will be taking a more modularized approach on a separate branch.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import SiteHeader from './Components/SiteHeader';
+import Profile from './Components/Profile';
+import AnimeBackground from './Components/AnimeBackground';
+import DataRef from './Components/DataRef';
+import InfoCard from './Components/Tabbing/InfoCard';
+import { Provider, connect } from 'react-redux';
+
+
+import store from './store';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+	<div className="topLevel">
+		<Provider store={store}>
+			<DataRef />
+		    <div className="App">
+		    	<SiteHeader />
+		    	<Profile />
+		    	<InfoCard />
+		    </div>
+	    </Provider>
     </div>
   );
 }
