3cd4317707cce58cf1376ebd0b38d9a9690756ee
Zerg <amcfarlane52@gmail.com>
2019-10-08 15:44:17 -0700

    cleaned up codebase to adhere to open-closed principle

--- src/Factory/ButtonFactory.js
+++ src/Factory/ButtonFactory.js
@@ -1,43 +1,53 @@
-import React from 'react';
+import React from "react";
 
-import { IconButton } from '@material-ui/core';
+import { createStyles } from "@material-ui/styles";
+import { IconButton } from "@material-ui/core";
 
-const visitPage = (url) => {
-	window.location = url;
-}
+import { SmallIconColor } from "../Constants";
 
-const mailReq = (address) => {
-	window.open('mailto:'.concat(address));
-}
+const visitPage = url => {
+  window.location = url;
+};
 
-const download = (url) => {
-	const link = document.createElement('a');
-	link.href = url;
-	link.download = url.split('/').pop();
-	document.body.appendChild(link);
-	link.click();
-	document.body.removeChild(link);
-}
+const mailReq = address => {
+  window.open("mailto:".concat(address));
+};
+
+const download = url => {
+  const link = document.createElement("a");
+  link.href = url;
+  link.download = url.split("/").pop();
+  document.body.appendChild(link);
+  link.click();
+  document.body.removeChild(link);
+};
 
 export const generateButtons = (styles: any, buttonData: any) => {
-	return Object.keys(buttonData).map( button => {
-		if (buttonData[button].type === 'email') {
-			return (<IconButton  onClick={() => mailReq(buttonData[button].email)}>
-				<img src={buttonData[button].path}
-					 className={styles.smallIcon} />
-			</IconButton>);
-		}
-		else if (buttonData[button].type === 'link') {
-			return (<IconButton onClick={() => visitPage(buttonData[button].link)}>
-				<img src={buttonData[button].path}
-					 className={styles.smallIcon} />
-			</IconButton>);
-		}
-		else if (buttonData[button].type === 'download') {
-			return (<IconButton onClick={() => download(buttonData[button].location)}>
-				<img src={buttonData[button].path} 
-					 className={styles.smallIcon} />
-			</IconButton>);
-		}
-	});
-}
+  return Object.keys(buttonData).map(button => {
+    const clickEvt = btnObj => {
+      switch (btnObj.type) {
+        case "email":
+          return mailReq(btnObj.email);
+        case "link":
+          return visitPage(btnObj.link);
+        case "download":
+          return download(btnObj.download);
+        default:
+          return null;
+      }
+    };
+    return (
+      <IconButton
+        onClick={() => {
+          clickEvt(buttonData[button]);
+        }}
+      >
+        <img
+          src={buttonData[button].path}
+          alt={buttonData[button].alt}
+          className={styles.smallIcon}
+        />
+      </IconButton>
+    );
+  });
+};
