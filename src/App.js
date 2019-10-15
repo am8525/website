4284a7cfe167e2ce6a968a71b05cc32ed454920c
Zerg <amcfarlane52@gmail.com>
2019-10-15 16:30:17 -0700

    including vanta.js for background anim

--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
-import React from 'react';
-import './App.css';
-import Background from './Components/Background';
-import SiteHeader from './Components/SiteHeader';
-import Profile from './Components/Profile';
-import DataRef from './Components/DataRef';
-import InfoCard from './Components/InfoCard';
-import { Provider } from 'react-redux';
+import React from "react";
+import "./App.css";
+import Background from "./Components/Background";
+import SiteHeader from "./Components/SiteHeader";
+import Profile from "./Components/Profile";
+import DataRef from "./Components/DataRef";
+import InfoCard from "./Components/InfoCard";
+import { Provider } from "react-redux";
 
-import store from './store';
+import store from "./store";
 
 function App() {
   return (
-	<div className="topLevel">
-		<Provider store={store}>
-			<DataRef />
-		    <div className="App">
-		    	<Background />
-		    	<SiteHeader />
-		    	<Profile />
-		    	<InfoCard />
-		    </div>
-	    </Provider>
+    <div className="topLevel">
+      <Provider store={store}>
+        <DataRef />
+        <div className="App">
+          <SiteHeader />
+          <Profile />
+          <InfoCard />
+          <Background></Background>
+        </div>
+      </Provider>
     </div>
   );
 }
