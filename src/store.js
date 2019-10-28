a220d3db111b8ed4065c59cb6e432a71e72b53e4
Zerg <amcfarlane52@gmail.com>
2019-10-27 19:54:25 -0700

    omitted chrome dev tools for non-chrome users. This should fix some source code bugs

--- src/store.js
+++ src/store.js
@@ -1,19 +1,31 @@
-import { createStore, applyMiddleware, compose } from 'redux';
-import thunk from 'redux-thunk';
+import { createStore, applyMiddleware, compose } from "redux";
+import thunk from "redux-thunk";
 
-import RootReducer from './Reducers/RootReducer';
+import RootReducer from "./Reducers/RootReducer";
 //need a rootReducer to import
 
 const initState = {};
 
 const middleware = [thunk];
 
-const store = createStore(RootReducer,
-						  initState,
-						  compose(
-						  	applyMiddleware(...middleware),
-						  	window.__REDUX_DEVTOOLS_EXTENSION__ &&
-						  	window.__REDUX_DEVTOOLS_EXTENSION__())
-						  );
+var store;
+
+if (window.navigator.userAgent.includes("Chrome")) {
+  store = createStore(
+    RootReducer,
+    initState,
+    compose(
+      applyMiddleware(...middleware),
+      window.__REDUX_DEVTOOLS_EXTENSION__ &&
+        window.__REDUX_DEVTOOLS_EXTENSION__()
+    )
+  );
+} else {
+  store = createStore(
+    RootReducer,
+    initState,
+    compose(applyMiddleware(...middleware))
+  );
+}
 
 export default store;
